0c73b0b185346a3c16ec097161584e35adf4f769
andreyvpb <andreyv.pb@outlook.com>
2026-09-16 18:12:31 +0200

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        size_t l = nums.size();
+        if (l <= 1) {
+            return static_cast<int>(l);
+        }
+
+        unordered_set<int> seen(l);
+        for (int n: nums) {
+            seen.insert(n);
+        }
+        
+        int max = 1;
+        for (int n: nums) {
+            int count = 1;
+            if (auto iter = seen.find(n); iter != seen.end()) {
+                    seen.erase(iter);
+            } else {
+                continue; // already found & removed
+            }
+            int backward = n - 1;   // TODO: potential issue - boudary check
+            do {
+                if (auto iter = seen.find(backward); iter != seen.end()) {
+                    --backward;
+                    ++count;
+                    seen.erase(iter);
+                } else {
+                    break;
+                }
+            } while (true);
+            int forward = n + 1;// TODO: potential issue - boudary check
+            do {
+                if (auto iter = seen.find(forward); iter != seen.end()) {
+                    ++forward;
+                    ++count;
+                    seen.erase(iter);
+                } else {
+                    break;
+                }
+            } while (true);
+            if (count > max) {
+                max = count;
+            }
+        }
+
+        return max;
+    }
+};
